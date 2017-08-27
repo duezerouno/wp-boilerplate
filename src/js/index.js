c1cdfed2894753509733e8d9914d2bf1af6f2838
mikehwagz <hmiklw@me.com>
2017-08-27 19:12:30 -0400

    add some scss boilerplate

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,11 @@
-// import scss entry point
 import '../scss/main.scss'
-
+import domready from 'domready'
 import module from './module'
 
 const fileName = 'index.js'
 const helloFrom = fileName => console.log(`hello from ${fileName}!`)
 
-helloFrom(fileName)
-helloFrom(module)
+domready(() => {
+  helloFrom(fileName)
+  helloFrom(module)
+})
