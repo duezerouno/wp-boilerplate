329a175780c7127fba8dddb4db689f64b1b87818
mikehwagz <hmiklw@me.com>
2017-08-31 16:12:33 -0400

    start app when dom is ready

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,5 @@
 import '../scss/main.scss'
 import domready from 'domready'
-import module from './module'
+import App from './app'
 
-const fileName = 'index.js'
-const helloFrom = fileName => console.log(`hello from ${fileName}!`)
-
-domready(() => {
-  helloFrom(fileName)
-  helloFrom(module)
-})
+domready(() => new App())
