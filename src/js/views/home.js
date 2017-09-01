ee8370e879f7c26ce07b4d9e95b2197f858aca07
mikehwagz <hmiklw@me.com>
2017-08-31 20:54:42 -0400

    remove test emit

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,9 +4,7 @@ import Emitter from '../core/emitter'
 const Home = Barba.BaseView.extend({
 	namespace: 'home',
 
-	onEnter () {
-    Emitter.emit('VIEW_READY', { namespace: this.namespace })
-	},
+	onEnter () {},
 
 	onEnterCompleted () {},
 
--- src/js/views/single.js
+++ src/js/views/single.js
@@ -4,9 +4,7 @@ import Emitter from '../core/emitter'
 const Single = Barba.BaseView.extend({
 	namespace: 'single',
 
-	onEnter () {
-    Emitter.emit('VIEW_READY', { namespace: this.namespace })
-	},
+	onEnter () {},
 
 	onEnterCompleted () {},
 
