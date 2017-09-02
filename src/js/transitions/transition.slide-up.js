33abe8eb42c47f4d0f506bd5703d25b2c9abb637
mikehwagz <hmiklw@me.com>
2017-09-01 20:44:05 -0400

    transition tweak

--- src/js/transitions/transition.slide-up.js
+++ src/js/transitions/transition.slide-up.js
@@ -1,7 +1,9 @@
 function slideUp (oldContainer, newContainer, done) {
+  const transform = 20
+
   function animateOut () {
     TweenLite.to(oldContainer, 0.5, {
-      y: -40,
+      y: -transform,
       autoAlpha: 0,
       onComplete: animateIn
     })
@@ -10,7 +12,7 @@ function slideUp (oldContainer, newContainer, done) {
   function animateIn () {
     return new TimelineLite({ onComplete: done })
       .set(oldContainer, { display: 'none' })
-      .set(newContainer, { autoAlpha: 0, y: 40 })
+      .set(newContainer, { autoAlpha: 0, y: transform })
       .to(newContainer, 0.7, { autoAlpha: 1, y: 0 })
   }
 
